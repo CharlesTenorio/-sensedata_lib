b7b78221352cc45b33d1ff06809ef0c9ba4a62d3
CharlesTenorio <charlestenorios@gmail.com>
2020-10-10 19:29:27 -0300

    implemetação do  Padrão Factory

--- libsensedata/swapi.py
+++ libsensedata/swapi.py
@@ -1,15 +1,32 @@
 import httpx
+from abc import ABCMeta, abstractclassmethod
+URL_BASE_API = 'https://swapi.dev/api/'
 
-URL_API = 'https://swapi.dev/api/'
+
+class UniversoStarWar(metaclass = ABCMeta):
+    def __init__(self):
+        self.url = URL_BASE_API
+        self.num_pagina = 10
+        
+    @abstractclassmethod
+    def retorna_lista(self, recurso):
+        pass
 
 
-class Personagens(object):
-    def __init__(self, num_paginas):
-        self.url = URL_API
-        self.paginacao = num_paginas
-
-    def lista_todos_personagens(self):
-        recurso = 'people/'
+class Personagens(UniversoStarWar):
+    def retorna_lista(self, recurso):
         url = self.url + recurso
-        list_todos = httpx.get(url)
-        return list_todos
+        list_personagen = httpx.get(url)
+        return list_personagen
+
+
+class Naves(UniversoStarWar):
+    def retorna_lista(self, recurso):
+        url = self.url + recurso
+        list_naves = httpx.get(url)
+        return list_naves
+
+## fabrica de objetos de starWar
+class StarWarFactory(object):
+    def criar_ojb_stawar(self, object_type, recurso):
+        return eval(object_type)().retorna_lista(recurso)        
--- libsensedata/tests/test_api.py
+++ libsensedata/tests/test_api.py
@@ -1,7 +1,15 @@
-from libsensedata.swapi import Personagens
+from libsensedata.swapi import StarWarFactory
 
 
-def test_api_metodo_get():
-    api_presonagem = Personagens(10)
-    request = api_presonagem.lista_todos_personagens()
+def test_api_retorna_personagen():
+    fabrica = StarWarFactory()
+    objeto = 'Personagens'
+    request = fabrica.criar_ojb_stawar(objeto, 'people/')
+    assert request.status_code == 200
+
+
+def test_api_retorna_naves():
+    fabrica = StarWarFactory()
+    objeto = 'Naves'
+    request = fabrica.criar_ojb_stawar(objeto, 'starships/')
     assert request.status_code == 200
